d152cafed4804d1dcd9233cf1f9ff80657dd3c12
samuelebruni <samuele_bruni@hotmail.com>
2023-09-13 16:41:12 +0200

    end exercise

--- app.js
+++ app.js
@@ -3,7 +3,50 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        
+        newElement: '',
+       
+        elementsList: 
+        [
+          {
+            text: 'Comprare le penne',
+            done: true
+          },
+          {
+            text: 'Comprare il detersivo',
+            done: false
+          },
+          {
+            text: 'Prendere Mike da scuola',
+            done: true
+          },
+          {
+            text: 'Guardare la partita',
+            done: false
+          },
+          {
+            text: 'Programmare al pc',
+            done: false
+          },
+        ],
+
+      }
+    },
+    methods: {
+      pushFunction(){
+        let newTask = {
+          text: this.newElement,
+          done: false
+        };
+        if(this.newElement){
+          this.elementsList.push(newTask);
+          this.newElement = '';
+        }
+      },
+      removeFunction(index){
+       let removedElement  = this.elementsList.splice(index, 1);
+       this.elementsList = this.elementsList.filter(element => element !== removedElement)
+       console.log(removedElement);
       }
     }
   }).mount('#app')
